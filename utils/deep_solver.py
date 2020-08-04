866046e9f81ce3d86f7849bbf0e64668d099fa61
Matthew Finch <finchmf@Matthews-MBP.home>
2020-08-04 02:59:58 -0400

    _

---
+++ utils/deep_solver.py
@@ -0,0 +1,20 @@
+import keras
+from keras.layers import Activation
+from keras.layers import Conv2D, BatchNormalization, Dense, Flatten, Reshape
+
+def suduko_network():
+
+    model = keras.Sequential()
+
+    model.add(Conv2D(64, kernal_size=(3,3), activation='relu', padding='same', input_shape=(9,9,1)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, kernal_size=(3,3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(128, kernal_size=(1,1), activation='relu', padding='same'))
+
+    model.add(Flatten())
+    model.add(Dense(81*9))
+    model.add(Reshape(-1, 9))
+    model.add(Activation('softmax'))
+
+    return model
